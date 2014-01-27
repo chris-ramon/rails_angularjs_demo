427e74c508bc7329b6bdc6d05521b17f6bc0657e
Christian Ramon <rchristian.ramon@gmail.com>
2014-01-27 15:48:24 -0500

    adding comments as nested resource of posts.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Backend::Application.routes.draw do
-  resources :posts, defaults: {format: :json}
+  resources :posts, defaults: {format: :json} do
+    resources :comments, except: [:show, :edit], defaults: {format: :json}
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
