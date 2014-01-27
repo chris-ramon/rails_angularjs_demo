661dcd5d5f50475828d97ee6662b3065fa6c623e
Christian Ramon <rchristian.ramon@gmail.com>
2014-01-27 15:49:03 -0500

    now posts controllers has rspec tests

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,16 +1,23 @@
 class PostsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    render json: @posts, status: :ok
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+      post = Post.where(id:params[:id]).first
+      if post.present?
+        render json: post, status: :ok
+      else
+        render json: error_json, status:  :not_found
+    end
   end
 
   # GET /posts/new
@@ -26,29 +33,20 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      render json: success_json, status: :ok
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
   end
 
@@ -56,10 +54,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+    render json: success_json, status: :ok
   end
 
   private
