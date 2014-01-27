95618bf4be74bb7cae978148b61ccb4c8910890a
Christian Ramon <rchristian.ramon@gmail.com>
2014-01-27 15:43:08 -0500

    adding success_json, error_json and rescue_from ParameterMissing within ApplicationController.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def success_json
+    {sucess: 'OK'}
+  end
+
+  def error_json
+    {failed: 'FAILED'}
+  end
+  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+    error = {}
+    error[parameter_missing_exception.param] = ['parameter is required']
+    response = { errors: [error] }
+    render json: response, status: :unprocessable_entity
+  end
 end
